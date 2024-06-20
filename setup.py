eaafa8fa6d15dbc0127054b40443f6741a38dcf2
John Wesley Hostetter <jhostetter16@gmail.com>
2024-06-20 11:13:59 -0400

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""
+Set up the 'soft' computing Python library.
+"""
+
+from setuptools import setup
+
+setup(
+    name="mtimeline",
+    version="1.0",
+    author="John Wesley Hostetter",
+    author_email="jhostetter16@gmail.com",
+    packages=["mtimeline"],
+)
